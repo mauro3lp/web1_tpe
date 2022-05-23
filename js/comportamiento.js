adc8db584cd0fab5e4d7f3444161f7334eee7d9e
Martin Cappi Dahl <mcappi91@gmail.com>
2022-05-23 15:02:06 -0300

    document DOMContentLoaded

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -1,51 +1,53 @@
 'use strict';
-const inputCaptcha = document.querySelector('#captcha');
-const valorCaptcha1 = document.querySelector('#valorCaptcha1');
-const valorCaptcha2 = document.querySelector('#valorCaptcha2');
-const btnRegistrar = document.querySelector('#registrarse');
+document.addEventListener('DOMContentLoaded', () => {
+  const inputCaptcha = document.querySelector('#captcha');
+  const valorCaptcha1 = document.querySelector('#valorCaptcha1');
+  const valorCaptcha2 = document.querySelector('#valorCaptcha2');
+  const btnRegistrar = document.querySelector('#registrarse');
 
-function obtenerValorAleatorio(listaValores) {
-  return listaValores[Math.floor(Math.random() * listaValores.length)];
-}
-
-function generarCaptcha() {
-  const valores = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  valorCaptcha1.innerHTML = obtenerValorAleatorio(valores);
-  valorCaptcha2.innerHTML = obtenerValorAleatorio(valores);
-}
-
-function validarCaptcha() {
-  const sumaCaptcha =
-    parseInt(valorCaptcha1.innerHTML) + parseInt(valorCaptcha2.innerHTML);
-  const resultadoCaptcha = parseInt(inputCaptcha.value);
-
-  const validacion = sumaCaptcha === resultadoCaptcha;
-  const captchaTieneSuccess = inputCaptcha.classList.contains('success');
-
-  if (validacion && !captchaTieneSuccess) {
-    inputCaptcha.classList.add('success');
-  } else if (captchaTieneSuccess) {
-    inputCaptcha.classList.remove('success');
+  function obtenerValorAleatorio(listaValores) {
+    return listaValores[Math.floor(Math.random() * listaValores.length)];
   }
 
-  return validacion;
-}
+  function generarCaptcha() {
+    const valores = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    valorCaptcha1.innerHTML = obtenerValorAleatorio(valores);
+    valorCaptcha2.innerHTML = obtenerValorAleatorio(valores);
+  }
 
-function toggleBotonRegistro(deshabilitado) {
-  btnRegistrar.disabled = deshabilitado;
-}
+  function validarCaptcha() {
+    const sumaCaptcha =
+      parseInt(valorCaptcha1.innerHTML) + parseInt(valorCaptcha2.innerHTML);
+    const resultadoCaptcha = parseInt(inputCaptcha.value);
 
-if (inputCaptcha) {
-  generarCaptcha();
+    const validacion = sumaCaptcha === resultadoCaptcha;
+    const captchaTieneSuccess = inputCaptcha.classList.contains('success');
 
-  inputCaptcha.addEventListener('input', (e) => {
-    e.preventDefault();
-    const validacion = validarCaptcha();
-    toggleBotonRegistro(!validacion);
-  });
+    if (validacion && !captchaTieneSuccess) {
+      inputCaptcha.classList.add('success');
+    } else if (captchaTieneSuccess) {
+      inputCaptcha.classList.remove('success');
+    }
 
-  btnRegistrar.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.location.href = './contacto.html';
-  });
-}
+    return validacion;
+  }
+
+  function toggleBotonRegistro(deshabilitado) {
+    btnRegistrar.disabled = deshabilitado;
+  }
+
+  if (inputCaptcha) {
+    generarCaptcha();
+
+    inputCaptcha.addEventListener('input', (e) => {
+      e.preventDefault();
+      const validacion = validarCaptcha();
+      toggleBotonRegistro(!validacion);
+    });
+
+    btnRegistrar.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.location.href = './contacto.html';
+    });
+  }
+}); // NO TIENE QUE QUEDAR NADA POR FUERA DE ESTE CIERRE DEL CALLBACK DE DOMCONTENTLOADED
